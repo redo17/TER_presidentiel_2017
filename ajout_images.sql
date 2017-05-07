cc2f9d0fc12f0f10e8bf8b934a11a83fecf07172
semrom <romain.semler@gmail.com>
2017-05-07 16:10:22 +0200

    Script SQL pour l'ajout des images des candidats en base.

---
+++ ajout_images.sql
@@ -0,0 +1,16 @@
+# Exécuter d'abord celle-ci :
+ALTER TABLE candidat
+ADD image_candidat varchar(255);
+
+# Puis ensuite celles-là :
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/692079601200295941/rCfOm6nc_400x400.jpg" WHERE `id_twitter_candidat`=1003575248;
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/845387075486728193/6HA8xTgS.jpg" WHERE `id_twitter_candidat`=200659061;
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/2511020171/orf7onnkacicyn8odg26.jpeg" WHERE `id_twitter_candidat`=150201042;
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/672054572601483265/xRGGJGn1.jpg" WHERE `id_twitter_candidat`=38170599;
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/845229246280863745/U3jdwWGz.jpg" WHERE `id_twitter_candidat`=551669623;
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/846787408712646657/cxRnGUlS.jpg" WHERE `id_twitter_candidat`=14389177;
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/847165852315201538/UD8nO_nH.jpg" WHERE `id_twitter_candidat`=102722347;
+UPDATE `candidat` SET `image_candidat`="https://candidats.media/static/images/candidates/9482374edecc8d9f2b2b749780741d2d.jpg" WHERE `id_twitter_candidat`=217749896;
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/834424630630817795/TfyS4uXb.jpg" WHERE `id_twitter_candidat`=1976143068;
+UPDATE `candidat` SET `image_candidat`="https://www.franceinter.fr/s3/cruiser-production/2017/04/ab054b0f-b4c6-4ad9-bdeb-01ad3e55b2a7/400x400_000_lz83e.jpg" WHERE `id_twitter_candidat`=80820758;
+UPDATE `candidat` SET `image_candidat`="https://pbs.twimg.com/profile_images/722778700136259585/SBDHHEwg.jpg" WHERE `id_twitter_candidat`=374392774;
